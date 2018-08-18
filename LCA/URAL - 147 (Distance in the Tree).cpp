0db8ad3afafa0a5191974a526b457a259dc5f6c6
LetsTrie <42464329+LetsTrie@users.noreply.github.com>
2018-08-19 01:10:04 +0600

    Create URAL - 147 (Distance in the Tree).cpp

---
+++ LCA/URAL - 147 (Distance in the Tree).cpp
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+
+#define fi first
+#define endl '\n'
+#define se second
+#define LL long long
+#define pb push_back
+#define vi vector<int>
+#define mod 1000000007
+#define EPS (double)1e-8
+#define pii pair<int , int>
+#define sz(x) (int) x.size()
+#define sl(x) scanf("%lld",&x)
+#define sii(x, y) si(x) , si(y)
+#define all(x) (x).begin() , (x).end()
+#define lg2(X) (31 - __builtin_clz(X))
+#define llg2(X) (63 - __builtin_clzll(X))
+#define rall(x) (x).rbegin() , (x).rend()
+#define siii(x, y, z) si(x) , si(y) , si(z)
+#define mem(a , b) memset((a), (b), sizeof(a))
+#define FOR(i , n) for(int i = 0 ; i < n ; i++)
+#define FOR1(i , n) for(int i = 1 ; i <= n ; i++)
+#define _ceil(n, a) ((n)%(a)==0?((n)/(a)):((n)/(a)+1))
+#define mlr int mid = (lo + hi) / 2 , l = node << 1 , r = l | 1 // 1 based
+#define itr(it, X) for (__typeof((X).begin()) it = (X).begin(); it != (X).end(); it++)
+template <typename T> T si(T &n){n = 0;bool neg = false;char c = getchar();while( c < '0' || c > '9'){if(c == '-') neg = true;c = getchar();}while(c >= '0' && c <= '9'){n = n*10 + c-48;c = getchar();}if(neg) n = ~(n-1);return n;}
+
+using namespace std;
+
+/*
+int fx[] = {1 , 0 , -1 , 0}; //, 1 , 1 , -1 , -1};
+int fy[] = {0 , 1 , 0 , -1}; //, 1 , -1 , 1 , -1};
+*/
+
+const int MAXN = 50000 + 10;
+int par[MAXN];
+vector<pii>G[MAXN];
+int depth[MAXN];
+bool vis[MAXN];
+int Table[MAXN][22];
+LL sum[MAXN][22];
+int ver;
+int Log[MAXN];
+int two[22];
+
+void dfs(int u = 0 , int d = 1) {
+	vis[u] = 1 , 
+	depth[u] = d;
+	for(int i = 0 ; i < G[u].size();  i++) {
+		int v = G[u][i].fi;
+		int w = G[u][i].se;
+		if(!vis[v]) {
+			par[v] = u;
+			sum[v][0] = w;
+			dfs(v , d + 1);
+		}
+	}
+}
+
+void lca() {
+	FOR(i , ver) Table[i][0] = par[i];
+	for(int i = 1 ; two[i] <= ver ; i++) {
+		for(int j = 0 ; j < ver ; j++) {
+			if(Table[j][i - 1] != -1) {
+				Table[j][i] = Table[ Table[j][i-1] ][i-1];
+				  sum[j][i] = 0LL + sum[j][i - 1] + sum[ Table[j][i-1] ][i - 1];
+			}
+		}
+	}
+}
+
+LL query(int x , int y) {
+	if(x == y) return 0LL;
+	int T;
+	LL Ans = 0;
+	if(depth[x] < depth[y]) T = x , x = y , y = T; 
+	for(int i = 21 ; i >= 0 ; i--) {
+		if(depth[x] - two[i] >= depth[y]) {
+			Ans += sum[x][i];
+			x = Table[x][i];
+		}
+	}
+	if(x == y) return Ans;
+	for(int i = 21 ; i >= 0 ; i--) {
+		if(Table[x][i] != Table[y][i] && Table[x][i] != -1) {
+			Ans += sum[x][i] , Ans += sum[y][i];
+			x = Table[x][i] , y = Table[y][i];
+		}
+	}
+	Ans += sum[x][0] , Ans += sum[y][0];
+	return Ans;
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+    	freopen("in.txt", "r", stdin);
+    	freopen("out.txt", "w", stdout);
+#endif
+    	mem(sum , 0);
+    	mem(Table, -1);
+    	Log[1] = 0, two[0] = 1;
+    	for(int i = 2 ; i < MAXN ; i++) Log[i] = Log[ i >> 1 ] + 1;
+    	for(int i = 1 ; i <= 21 ; i++) two[i] = two[i - 1] << 1;
+    	si(ver); ver--;
+    	FOR(i , ver) {
+    		int x , y, z;
+    		siii(x , y , z);
+    		G[x].pb({y , z});
+    		G[y].pb({x , z});
+    	} ver++;
+    	FOR1(i , ver) par[i] = -1;
+    	dfs(); 
+    	lca();
+    	int Q;
+    	si(Q);
+    	static int ks = 1;
+    	while(Q--) {
+    		int x , y;
+    		sii(x , y);
+    		printf("%lld\n", query(x , y));
+    	}
+}
